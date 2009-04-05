2e320886c218e592fa1f79bb2f50725e290a1023
artemave <artemave@gmail.com>
2009-04-05 16:04:54 -0400

    setting up deployment with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,13 @@
+set :user, 'artemave'
+ssh_options[:keys] = %w{/home/artem/.ssh/id_rsa}
+set :domain, 'mystique.dreamhost.com'
+set :project, 'weatherdiff'
 set :application, "weatherdiff.com"
-set :repository,  "git://github.com/artemave/weatherdiff.git"
+set :applicationdir, "/home/#{user}/#{application}"
+
+set :scm, 'git'
+set :repository, "git://github.com/artemave/weatherdiff.git"
+set :branch, 'master'
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -10,6 +18,22 @@ set :repository,  "git://github.com/artemave/weatherdiff.git"
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "mystique.dreamhost.com"
-role :web, "mystique.dreamhost.com"
-role :db,  "mysql.weatherdiff.com", :primary => true
+role :app, domain
+role :web, domain
+role :db,  domain, :primary => true
+
+set :deploy_to, applicationdir
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+namespace :deploy do
+	task :restart do
+		run "touch #{current_path}/tmp/restart.txt"
+	end
+
+	task :load_submodules do
+		run "cd #{current_path}; git submodule update --init"
+	end
+end
+
+after "deploy:update", "deploy:load_submodules"

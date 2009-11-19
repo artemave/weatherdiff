9606d80c6c34d1773fc3e80763d30934531e5236
artemave <artemave@gmail.com>
2009-11-19 06:32:01 -0500

    removing hirb dependency

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,3 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
-
-require 'hirb'
-require 'pp'
-
-Hirb::View.enable

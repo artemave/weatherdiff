945385aad7c19774c0fb9a7fa282d1fdbe17ab15
artemave <artemave@gmail.com>
2009-12-04 15:34:01 +0300

    zoom/pan added

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,11 +10,13 @@ class Report
 
   def generate_flot_data
     ls = Location.with_samples.find(:all, :conditions => ['name in (?,?)', location1, location2]);
-    max_y, min_y = nil, nil
+    max_y, min_y, min_ts, max_ts = nil, nil, nil, nil
     @flot[:data] = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = [] } }
     ls.each do |loc|
       for ss in loc.sample_summaries
         ts = Time.gm(ss.rss_ts.year, ss.rss_ts.month, ss.rss_ts.day).to_i * 1000 # flot likes miliseconds
+        max_ts = ts unless max_ts and max_ts > ts
+        min_ts = ts unless min_ts and min_ts < ts
         
         @flot[:data][loc.name][:max_temp] << [ts, ss.max_temp]
         max_y = ss.max_temp.to_i unless max_y and max_y > ss.max_temp.to_i
@@ -31,6 +33,9 @@ class Report
       {:key => :max_temp, :name => 'Maximum temperature', :min_y => min_y, :max_y => max_y },
       {:key => :min_temp, :name => 'Minimum temperature', :min_y => min_y, :max_y => max_y },
     ]
+
+    @flot[:zoom_range] = [1000 * 60 * 60 * 24 * 7, max_ts - min_ts]; # week to max period
+    @flot[:pan_range] = [min_ts, max_ts];
   end
 
   # a secret kick for restful routes to work

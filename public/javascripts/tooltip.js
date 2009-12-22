4d9b226c283c4c06741d1abec6f6e74f4bab29f9
artemave <artemave@gmail.com>
2009-12-22 14:23:31 +0300

    made tooltip opacity IE friendly

--- public/javascripts/tooltip.js
+++ public/javascripts/tooltip.js
@@ -1,11 +1,15 @@
 function showTooltip(x, y, contents, css_class) {
-  $('<div class="site-tooltip">' + contents + '</div>').css( {
+  $('<div class="site-tooltip">' + contents + '</div>')
+  .css({
     position: 'absolute',
     display: 'none',
     top: y + 5,
-    left: x + 15,
-    opacity: 0.80
-  }).addClass(css_class == undefined ? 'ui-state-highlight' : '').appendTo("body").fadeIn(200);
+    left: x + 15
+  })
+  .css($.support.opacity ? {opacity: 0.8} : {}) //guess which browser does not support opacity? the answer is amaizingly obvious.
+  .addClass(css_class == undefined ? 'ui-state-highlight' : '')
+  .appendTo("body")
+  .show();
 }
 
 function removeTooltip()  {

d136cfddcbfb9f66f18e8d6c6d9dbf740c1398a5
artemave <artemave@gmail.com>
2009-12-22 16:32:49 +0300

    hoptoad added

--- test/unit/location_test.rb
+++ test/unit/location_test.rb
@@ -3,9 +3,6 @@ require File.dirname(__FILE__)+'/../test_helper'
 class LocationTest < ActiveSupport::TestCase
 	fixtures :locations
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
 
 	test "new location" do
 		p = Location.new :name => 'France, Paris', :feed => 'http://feeds.bbc.co.uk/weather/feeds/rss/5day/world/40.xml', :tz => 'Paris'
@@ -14,7 +11,7 @@ class LocationTest < ActiveSupport::TestCase
 
 	test "is location sampled?" do
 		p = Location.new :name => 'France, Paris', :feed => 'http://feeds.bbc.co.uk/weather/feeds/rss/5day/world/40.xml', :tz => 'Paris'
-		assert !p.already_sampled?(Date.today), 'No samples for today'
+		assert !p.already_sampled?(Time.now.in_time_zone('Paris')), 'No samples for today'
 	end
 
 	test "sample" do
@@ -22,6 +19,6 @@ class LocationTest < ActiveSupport::TestCase
 		p.save
 		p.sample
 		assert p.samples.count == 3, "#{p.samples.count} samples taken"
-		assert p.already_sampled?(Date.today), 'Today is already sampled'
+		assert p.already_sampled?(Time.now.in_time_zone('Paris')), 'Today is already sampled'
 	end
 end

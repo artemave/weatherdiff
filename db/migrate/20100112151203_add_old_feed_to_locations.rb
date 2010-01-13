1c0256beed5874cd7d013dece6b1098df70e0c09
artemave <artemave@gmail.com>
2010-01-13 13:06:13 +0300

    fixed migration: ruby references are odd

--- db/migrate/20100112151203_add_old_feed_to_locations.rb
+++ db/migrate/20100112151203_add_old_feed_to_locations.rb
@@ -2,15 +2,15 @@ class AddOldFeedToLocations < ActiveRecord::Migration
   def self.up
     add_column :locations, :old_feed, :string
     Location.all.each do |l|
-      l.old_feed = l.feed
-      l.feed.sub!(/^.*?(\d+)\.xml$/, 'http://newsrss.bbc.co.uk/weather/forecast/\1/Next3DaysRSS.xml')
+      l.old_feed = l.feed.clone
+      l.feed = l.feed.sub(/^.*?(\d+)\.xml$/, 'http://newsrss.bbc.co.uk/weather/forecast/\1/Next3DaysRSS.xml')
       l.save!
     end
   end
 
   def self.down
     Location.all.each do |l|
-      l.feed = l.old_feed
+      l.feed = l.old_feed.clone
       l.save!
     end
     remove_column :locations, :old_feed

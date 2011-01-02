5745302fd8597088d41d3b810d54bb602cb4236c
artemave <artemave@gmail.com>
2011-01-02 19:20:39 +0000

    fixing cron sampling not working probably due to custom gem location

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 # Learn more: http://github.com/javan/whenever
 
+env :GEM_HOME, ENV['GEM_HOME'] || "GEM_HOME=#{ENV['HOME']}/.gems/"
+
 every :hour do
   runner 'Location.all.each {|l| begin; l.sample; rescue => e; Rails.logger.error e.backtrace.join("\n") end}'
 end

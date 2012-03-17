6d94cab919b4eb70e2902227233c319ea76a7292
artemave <artemave@gmail.com>
2012-03-17 16:04:35 +0000

    disable user registration in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,39 @@ class UsersController < ApplicationController
 	layout 'locations'
   # render new.rhtml
   def new
-    @user = User.new
+    if_registration_allowed do
+      @user = User.new
+    end
   end
  
   def create
-    logout_keeping_session!
-    @user = User.new(params[:user])
-    success = @user && @user.save
-    if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
-    else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-      render :action => 'new'
+    if_registration_allowed do
+      logout_keeping_session!
+      @user = User.new(params[:user])
+      success = @user && @user.save
+      if success && @user.errors.empty?
+        # Protects against session fixation attacks, causes request forgery
+        # protection if visitor resubmits an earlier form using back
+        # button. Uncomment if you understand the tradeoffs.
+        # reset session
+        self.current_user = @user # !! now logged in
+        redirect_back_or_default('/')
+        flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      else
+        flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+        render :action => 'new'
+      end
     end
   end
+
+  protected
+
+    def if_registration_allowed
+      if Rails.env.production?
+        flash[:error] = "Registration disabled"
+        redirect_back_or_default '/'
+      else
+        yield
+      end
+    end
 end
